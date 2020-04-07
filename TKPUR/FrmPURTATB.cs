414e6d19261228cf655270c5d504fb8277b4fd33
at3158 <tk290@tkfood.com.tw>
2020-04-07 16:38:24 +0800

    SETFASETSQL

--- TKPUR/FrmPURTATB.cs
+++ TKPUR/FrmPURTATB.cs
@@ -490,7 +490,7 @@ namespace TKPUR
             FASTSQL.AppendFormat(@"   SELECT CONVERT(NVARCHAR,[DATES],112) AS '日期',[TA001] AS '請購單別',[TA002] AS '請購單號',[VERSIONS] AS '修改次數',[COMMENT] AS '單頭備註', [ID]");
             FASTSQL.AppendFormat(@"   FROM [TKPUR].[dbo].[PURTATB]");
             FASTSQL.AppendFormat(@"   WHERE [ID]='{0}'", REPORTID);
-            FASTSQL.AppendFormat(@"   ");
+            FASTSQL.AppendFormat(@"     ");
 
             return FASTSQL.ToString();
         }
