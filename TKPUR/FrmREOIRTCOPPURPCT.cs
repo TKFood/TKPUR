de8c03d104b7c820fb9f8a802b8737da4de54b6a
at3158 <tk290@tkfood.com.tw>
2020-10-22 11:47:13 +0800

    FrmREOIRTCOPPURPCT

---
+++ TKPUR/FrmREOIRTCOPPURPCT.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+using NPOI.SS.UserModel;
+using System.Configuration;
+using NPOI.XSSF.UserModel;
+using NPOI.SS.Util;
+using System.Reflection;
+using System.Threading;
+using FastReport;
+using FastReport.Data;
+
+namespace TKPUR
+{
+    public partial class FrmREOIRTCOPPURPCT : Form
+    {
+        SqlConnection sqlConn = new SqlConnection();
+        SqlCommand sqlComm = new SqlCommand();
+        string connectionString;
+        StringBuilder sbSql = new StringBuilder();
+        StringBuilder sbSqlQuery = new StringBuilder();
+        SqlDataAdapter adapter1 = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder1 = new SqlCommandBuilder();
+        DataSet ds = new DataSet();
+
+        SqlTransaction tran;
+        SqlCommand cmd = new SqlCommand();
+        
+
+        string tablename = null;
+        string EDITID;
+        int result;
+        Thread TD;
+
+        public FrmREOIRTCOPPURPCT()
+        {
+            InitializeComponent();
+
+            SETDATES();
+        }
+
+        #region FUNCTION
+        public void SETDATES()
+        {
+            dateTimePicker1.Value = DateTime.Now;
+            dateTimePicker2.Value = DateTime.Now;
+        }
+
+        #endregion
+
+        #region BUTTON
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+    }
+}
