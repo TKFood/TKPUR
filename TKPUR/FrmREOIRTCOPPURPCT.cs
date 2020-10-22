2a08b7d30c46837434dc9479683f2a8a064126ff
at3158 <tk290@tkfood.com.tw>
2020-10-22 15:50:04 +0800

    SERACHCOPPURPCTPTOADDORUPDATE

--- TKPUR/FrmREOIRTCOPPURPCT.cs
+++ TKPUR/FrmREOIRTCOPPURPCT.cs
@@ -188,6 +188,162 @@ namespace TKPUR
             }
         }
 
+
+        public void SERACHCOPPURPCTPTOADDORUPDATE()
+        {
+            SqlDataAdapter adapter1 = new SqlDataAdapter();
+            SqlCommandBuilder sqlCmdBuilder1 = new SqlCommandBuilder();
+            DataSet ds = new DataSet();
+
+
+            ds.Clear();
+
+            try
+            {
+                connectionString = ConfigurationManager.ConnectionStrings["dberp"].ConnectionString;
+                sqlConn = new SqlConnection(connectionString);
+
+                sbSql.Clear();
+                sbSqlQuery.Clear();
+
+
+                sbSql.AppendFormat(@"
+                                    SELECT *
+                                    FROM [TKPUR].[dbo].[COPPURPCT]
+                                    WHERE [YM]='{0}'
+                                    ", dateTimePicker4.Value.ToString("yyyyMM"));
+
+                adapter1 = new SqlDataAdapter(@"" + sbSql, sqlConn);
+
+                sqlCmdBuilder1 = new SqlCommandBuilder(adapter1);
+                sqlConn.Open();
+                ds.Clear();
+                adapter1.Fill(ds, "TEMPds1");
+                sqlConn.Close();
+
+
+                if (ds.Tables["TEMPds1"].Rows.Count == 0)
+                {
+                    ADDCOPPURPCT(dateTimePicker4.Value.ToString("yyyyMM"),textBox1.Text.Trim(), textBox2.Text.Trim(), textBox3.Text.Trim());
+                }
+                else
+                {
+                    if (ds.Tables["TEMPds1"].Rows.Count >= 1)
+                    {
+                        UPDATECOPPURPCT(dateTimePicker4.Value.ToString("yyyyMM"), textBox1.Text.Trim(), textBox2.Text.Trim(), textBox3.Text.Trim());
+
+                    }
+
+                }
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+        }
+
+        public void ADDCOPPURPCT(string YM,string PURMONEY1,string PURMONEY2,string COPMONEY)
+        {
+            try
+            {
+                connectionString = ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString;
+                sqlConn = new SqlConnection(connectionString);
+
+                sqlConn.Close();
+                sqlConn.Open();
+                tran = sqlConn.BeginTransaction();
+
+                sbSql.Clear();
+               
+                sbSql.AppendFormat(@" 
+                                    INSERT INTO [TKPUR].[dbo].[COPPURPCT]
+                                    ([YM],[PURMONEY1],[PURMONEY2],[COPMONEY])
+                                    VALUES
+                                    ('{0}',{1},{2},{3})
+                                    ", YM, PURMONEY1, PURMONEY2, COPMONEY);
+
+
+                cmd.Connection = sqlConn;
+                cmd.CommandTimeout = 60;
+                cmd.CommandText = sbSql.ToString();
+                cmd.Transaction = tran;
+                result = cmd.ExecuteNonQuery();
+
+                if (result == 0)
+                {
+                    tran.Rollback();    //交易取消
+                }
+                else
+                {
+                    tran.Commit();      //執行交易                   
+
+                }
+
+            }
+            catch
+            {
+
+            }
+
+            finally
+            {
+                sqlConn.Close();
+            }
+        }
+
+        public void UPDATECOPPURPCT(string YM, string PURMONEY1, string PURMONEY2, string COPMONEY)
+        {
+            try
+            {
+                connectionString = ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString;
+                sqlConn = new SqlConnection(connectionString);
+
+                sqlConn.Close();
+                sqlConn.Open();
+                tran = sqlConn.BeginTransaction();
+
+                sbSql.Clear();
+
+                sbSql.AppendFormat(@" 
+                                    UPDATE [TKPUR].[dbo].[COPPURPCT]
+                                    SET [PURMONEY1]={1},[PURMONEY2]={2},[COPMONEY]={3}
+                                    WHERE [YM]='{0}'
+                                    ", YM, PURMONEY1, PURMONEY2, COPMONEY);
+
+
+                cmd.Connection = sqlConn;
+                cmd.CommandTimeout = 60;
+                cmd.CommandText = sbSql.ToString();
+                cmd.Transaction = tran;
+                result = cmd.ExecuteNonQuery();
+
+                if (result == 0)
+                {
+                    tran.Rollback();    //交易取消
+                }
+                else
+                {
+                    tran.Commit();      //執行交易                   
+
+                }
+
+            }
+            catch
+            {
+
+            }
+
+            finally
+            {
+                sqlConn.Close();
+            }
+        }
+
         #endregion
 
         #region BUTTON
@@ -199,7 +355,10 @@ namespace TKPUR
         private void button4_Click(object sender, EventArgs e)
         {
             SERACHPUR();
-            SERACHCOP();        }
+            SERACHCOP();
+
+            SERACHCOPPURPCTPTOADDORUPDATE();
+        }
 
         #endregion
 
