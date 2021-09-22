195d9dcf06fa707e410bc9ff8906e3eceba73f1b
at3158 <tk290@tkfood.com.tw>
2021-09-22 16:22:10 +0800

    FrmREPORTPURTLTM

--- TKPUR/FrmREPORTPURTLTM.cs
+++ TKPUR/FrmREPORTPURTLTM.cs
@@ -17,6 +17,7 @@ using System.Reflection;
 using System.Threading;
 using FastReport;
 using FastReport.Data;
+using TKITDLL;
 
 namespace TKPUR
 {
@@ -63,7 +64,21 @@ namespace TKPUR
             report1 = new Report();
             report1.Load(@"REPORT\原料、物料核價單漲跌整理表.frx");
 
-            report1.Dictionary.Connections[0].ConnectionString = ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString;
+            //20210902密
+            Class1 TKID = new Class1();//用new 建立類別實體
+            SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString);
+
+            //資料庫使用者密碼解密
+            sqlsb.Password = TKID.Decryption(sqlsb.Password);
+            sqlsb.UserID = TKID.Decryption(sqlsb.UserID);
+
+            String connectionString;
+            sqlConn = new SqlConnection(sqlsb.ConnectionString);
+
+            report1.Dictionary.Connections[0].ConnectionString = sqlsb.ConnectionString;
+
+
+            
             //report1.Dictionary.Connections[0].ConnectionString = "server=192.168.1.105;database=TKPUR;uid=sa;pwd=dsc";
 
             TableDataSource Table = report1.GetDataSource("Table") as TableDataSource;
