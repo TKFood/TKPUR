785e271bdac8e99598037023df34dd6e5e25d0f3
at3158 <tk290@tkfood.com.tw>
2021-09-27 17:42:51 +0800

    SERACHlDOC_NBR

--- TKPUR/FrmPURTATBUOFCHANGE.cs
+++ TKPUR/FrmPURTATBUOFCHANGE.cs
@@ -1538,7 +1538,7 @@ namespace TKPUR
             {
                 //20210902密
                 Class1 TKID = new Class1();//用new 建立類別實體
-                SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString);
+                SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["dbUOF"].ConnectionString);
 
                 //資料庫使用者密碼解密
                 sqlsb.Password = TKID.Decryption(sqlsb.Password);
