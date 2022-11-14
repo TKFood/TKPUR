5e7aa1049b36895521c5867132a9f0853f406851
at3158 <tk290@tkfood.com.tw>
2022-11-14 15:28:23 +0800

    ADDV frmREPORTFORMPURTEPURTF

---
+++ TKPUR/frmREPORTFORMPURTEPURTF.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+using NPOI.SS.UserModel;
+using System.Configuration;
+using NPOI.XSSF.UserModel;
+using NPOI.SS.Util;
+using System.Reflection;
+using System.Threading;
+using FastReport;
+using FastReport.Data;
+using TKITDLL;
+using FastReport.Export.Pdf;
+using System.Net.Mail;
+using System.Net.Mime;
+
+namespace TKPUR
+{
+    public partial class frmREPORTFORMPURTEPURTF : Form
+    {
+        SqlConnection sqlConn = new SqlConnection();
+        SqlCommand sqlComm = new SqlCommand();
+        string connectionString;
+        StringBuilder sbSql = new StringBuilder();
+        StringBuilder sbSqlQuery = new StringBuilder();
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder = new SqlCommandBuilder();
+        SqlDataAdapter adapter2 = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder2 = new SqlCommandBuilder();
+        SqlDataAdapter adapter4 = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder4 = new SqlCommandBuilder();
+        SqlTransaction tran;
+        SqlCommand cmd = new SqlCommand();
+        DataSet ds = new DataSet();
+        DataSet ds2 = new DataSet();
+        DataSet ds4 = new DataSet();
+
+        DataSet DSPURTCTD = new DataSet();
+
+        DataTable dt = new DataTable();
+        string tablename = null;
+        string EDITID;
+        int result;
+        Thread TD;
+
+        Report report1 = new Report();
+        string MAILATTACHPATH = null;
+        DataSet DSMAIL = new DataSet();
+
+        public frmREPORTFORMPURTEPURTF()
+        {
+            InitializeComponent();
+        }
+
+
+        #region FUNCTION
+
+
+        #endregion
+
+        #region BUTTON
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        #endregion
+    }
+}
