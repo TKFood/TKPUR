28cce6db715dddb0d7311734ea4991bf1fca3c1b
at3158 <tk290@tkfood.com.tw>
2023-12-18 14:58:07 +0800

    SEARCH_PURVERSIONSNUMS

---
+++ TKPUR/frmPURVERSIONSNUMS.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+using NPOI.SS.UserModel;
+using System.Configuration;
+using NPOI.XSSF.UserModel;
+using NPOI.SS.Util;
+using System.Reflection;
+using System.Threading;
+using FastReport;
+using FastReport.Data;
+using System.Xml;
+using TKITDLL;
+namespace TKPUR
+{
+    public partial class frmPURVERSIONSNUMS : Form
+    {
+        SqlConnection sqlConn = new SqlConnection();
+        SqlCommand sqlComm = new SqlCommand();
+        string connectionString;
+        StringBuilder sbSql = new StringBuilder();
+        StringBuilder sbSqlQuery = new StringBuilder();
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder = new SqlCommandBuilder();
+
+        public frmPURVERSIONSNUMS()
+        {
+            InitializeComponent();
+        }
+
+        #region FUNCTION
+        public void SEARCH_PURVERSIONSNUMS(string NAMES, string MB001,string ISCLOSE)
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            SqlCommandBuilder sqlCmdBuilder = new SqlCommandBuilder();
+
+            SqlTransaction tran;
+            SqlCommand cmd = new SqlCommand();
+            DataSet ds = new DataSet();
+
+            try
+            {
+                //20210902密
+                Class1 TKID = new Class1();//用new 建立類別實體
+                SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString);
+
+                //資料庫使用者密碼解密
+                sqlsb.Password = TKID.Decryption(sqlsb.Password);
+                sqlsb.UserID = TKID.Decryption(sqlsb.UserID);
+
+                String connectionString;
+                sqlConn = new SqlConnection(sqlsb.ConnectionString);
+
+
+
+                sbSql.Clear();
+                sbSqlQuery.Clear();
+
+                sbSql.AppendFormat(@"
+                                    SELECT 
+                                     [NAMES] AS '版型' 
+                                    ,[MB001] AS '品琥' 
+                                    ,[MB002] AS '品名' 
+                                    ,[BACKMONEYS] AS '可退還的版費' 
+                                    ,[TARGETNUMS] AS '目標進貨量' 
+                                    ,[TOTALNUMS] AS '已進貨量' 
+                                    ,[ISCLOSE] AS '是否結案' 
+                                    FROM [TKPUR].[dbo].[PURVERSIONSNUMS]
+                                    ", NAMES, MB001);
+
+                adapter = new SqlDataAdapter(@"" + sbSql, sqlConn);
+
+                sqlCmdBuilder = new SqlCommandBuilder(adapter);
+                sqlConn.Open();
+                ds.Clear();
+                adapter.Fill(ds, "ds");
+                sqlConn.Close();
+
+                if (ds.Tables["ds"].Rows.Count >= 1)
+                {
+                    dataGridView1.DataSource = ds.Tables["ds"];
+                    dataGridView1.AutoResizeColumns();
+
+                }              
+                else
+                {
+                    dataGridView1.DataSource = null;
+
+                }
+
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+        }
+
+        #endregion
+
+        #region BUTTON
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SEARCH_PURVERSIONSNUMS("'","","N");
+        }
+
+        #endregion
+    }
+}
