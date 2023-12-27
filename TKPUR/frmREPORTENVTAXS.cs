5a2e45a74c1129356902cc76f5d5b7cc874e2981
at3158 <tk290@tkfood.com.tw>
2023-12-27 11:12:02 +0800

    SETFASTREPORT

---
+++ TKPUR/frmREPORTENVTAXS.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NPOI;
+using NPOI.HPSF;
+using NPOI.HSSF;
+using NPOI.HSSF.UserModel;
+using NPOI.POIFS;
+using NPOI.Util;
+using NPOI.HSSF.Util;
+using NPOI.HSSF.Extractor;
+using System.IO;
+using System.Data.SqlClient;
+using NPOI.SS.UserModel;
+using System.Configuration;
+using NPOI.XSSF.UserModel;
+using System.Text.RegularExpressions;
+using FastReport;
+using FastReport.Data;
+using TKITDLL;
+
+namespace TKPUR
+{
+    public partial class frmREPORTENVTAXS : Form
+    {
+        SqlConnection sqlConn = new SqlConnection();
+        SqlCommand sqlComm = new SqlCommand();
+        string connectionString;
+        StringBuilder sbSql = new StringBuilder();
+        StringBuilder sbSqlQuery = new StringBuilder();
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder = new SqlCommandBuilder();
+        SqlTransaction tran;
+        SqlCommand cmd = new SqlCommand();
+        DataSet ds = new DataSet();
+        DataSet ds2 = new DataSet();
+        string tablename = null;
+        int rownum = 0;
+        DataGridViewRow row;
+        string SALSESID = null;
+        int result;
+
+        public Report report1 { get; private set; }
+
+        public frmREPORTENVTAXS()
+        {
+            InitializeComponent();
+        }
+
+        #region FUNCTION
+        public void SETFASTREPORT(string YYYYMM)
+        {
+            string YY = YYYYMM.Substring(0,4);
+            string MM = YYYYMM.Substring(4,2);
+
+            string SQL;
+            string SQL1;
+            report1 = new Report();
+            report1.Load(@"REPORT\環保稅.frx");
+
+            //20210902密
+            Class1 TKID = new Class1();//用new 建立類別實體
+            SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString);
+
+            //資料庫使用者密碼解密
+            sqlsb.Password = TKID.Decryption(sqlsb.Password);
+            sqlsb.UserID = TKID.Decryption(sqlsb.UserID);
+
+            String connectionString;
+            sqlConn = new SqlConnection(sqlsb.ConnectionString);
+
+            report1.Dictionary.Connections[0].ConnectionString = sqlsb.ConnectionString;
+
+
+            //report1.Dictionary.Connections[0].ConnectionString = "server=192.168.1.105;database=TKPUR;uid=sa;pwd=dsc";
+
+            TableDataSource Table = report1.GetDataSource("Table") as TableDataSource;
+            SQL = SETFASETSQL(YY,MM);
+            Table.SelectCommand = SQL;
+            TableDataSource Table1 = report1.GetDataSource("Table1") as TableDataSource;
+            SQL1= SETFASETSQL1(YY, MM);
+            Table1.SelectCommand = SQL1;
+            report1.Preview = previewControl1;
+            report1.Show();
+
+        }
+
+        public string SETFASETSQL(string YY,string MM)
+        {
+            StringBuilder FASTSQL = new StringBuilder();
+            StringBuilder STRQUERY = new StringBuilder();
+         
+            FASTSQL.AppendFormat(@"  
+                                SELECT SUBSTRING(TG003,1,4) AS '年',SUBSTRING(TG003,5,2)  AS '月',TG005 AS '廠商代',MA002 AS '廠商',MA005 AS '統編',TH004 AS '品號',MB002 AS '品名',SUM(TH015)  AS '進貨驗收數量',TH008 AS '單位'
+                                FROM [TK].dbo.PURTG,[TK].dbo.PURTH, [TK].dbo.PURMA, [TK].dbo.INVMB 
+                                WHERE TG001=TH001 AND TG002=TH002
+                                AND MA001=TG005
+                                AND MB001=TH004
+                                AND TG013='Y'
+                                AND TH004 LIKE '205%'
+                                AND SUBSTRING(TG003,1,4)='{0}'
+                                AND SUBSTRING(TG003,5,2)='{1}'
+                                GROUP BY  SUBSTRING(TG003,1,4),SUBSTRING(TG003,5,2),TG005,MA002,MA005,TH004,MB002,MB003,TH008
+                                ORDER BY  SUBSTRING(TG003,1,4),SUBSTRING(TG003,5,2),TG005,MA002,MA005,TH004,MB002,MB003,TH008
+                                    ",YY,MM);
+
+            return FASTSQL.ToString();
+        }
+
+        public string SETFASETSQL1(string YY, string MM)
+        {
+            StringBuilder FASTSQL = new StringBuilder();
+            StringBuilder STRQUERY = new StringBuilder();
+
+            FASTSQL.AppendFormat(@"                                
+                                SELECT  SUBSTRING(TG003,1,4) AS '年',SUBSTRING(TG003,5,2)  AS '月',TG004 AS '客戶代',MA002 AS '客戶',MA010 AS '統編',TH004 ,MB1.MB002 ,SUM(LA011),MB1.MB004,MC004,MD006,MD007,MD003 AS '品號',MB2.MB002 AS '品名',SUM(CONVERT(DECIMAL(16,0),(LA011/MD006*MD007*MC004)))  AS '數量',MB2.MB004 AS '單位'
+                                FROM [TK].dbo.COPTG,[TK].dbo.COPTH,[TK].dbo.INVLA,[TK].dbo.INVMB MB1,[TK].dbo.COPMA,[TK].dbo.BOMMC,[TK].dbo.BOMMD,[TK].dbo.INVMB MB2
+                                WHERE TG001=TH001 AND TG002=TH002
+                                AND LA006=TH001 AND LA007=TH002 AND LA008=TH003
+                                AND TH004=MB1.MB001
+                                AND TG004=MA001
+                                AND MC001=TH004
+                                AND MC001=MD001
+                                AND MD003=MB2.MB001
+                                AND MD003 LIKE '205%'
+                                AND MD035 NOT LIKE '%蓋%'
+                                AND (TG004 LIKE '2%' OR TG004 LIKE 'A%')
+                                AND SUBSTRING(TG003,1,4)='{0}' AND SUBSTRING(TG003,5,2)='{1}'
+                                GROUP BY SUBSTRING(TG003,1,4),SUBSTRING(TG003,5,2),TG004,MA002,MA010,TH004,MB1.MB002,MB1.MB004,MC004,MD006,MD007,MD003,MB2.MB002,MB2.MB004
+                                ORDER BY SUBSTRING(TG003,1,4),SUBSTRING(TG003,5,2),TG004,MA002,MA010,TH004
+
+                                    ", YY, MM);
+
+            return FASTSQL.ToString();
+        }
+        #endregion
+
+        #region BUTTON
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SETFASTREPORT(dateTimePicker1.Value.ToString("yyyyMM"));
+        }
+
+        #endregion
+    }
+}
