601470b95cea94c198a770b32bc580392b7fe383
at3158 <tk290@tkfood.com.tw>
2024-09-27 11:16:49 +0800

    SETFASTREPORT2

--- TKPUR/frmREPORTENVTAXS.Designer.cs
+++ TKPUR/frmREPORTENVTAXS.Designer.cs
@@ -30,13 +30,17 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.previewControl1 = new FastReport.Preview.PreviewControl();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.button1 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.previewControl1 = new FastReport.Preview.PreviewControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
+            this.button2 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
@@ -68,16 +72,6 @@
             this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 26);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 70);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
             // splitContainer1
             // 
             this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
@@ -97,6 +91,64 @@
             this.splitContainer1.SplitterDistance = 107;
             this.splitContainer1.TabIndex = 0;
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.LightGreen;
+            this.tableLayoutPanel1.ColumnCount = 7;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 246F));
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dateTimePicker1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.dateTimePicker2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.dateTimePicker3, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 3, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(966, 103);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(243, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "查詢";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "年月";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "yyyy/MM";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(123, 3);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(114, 27);
+            this.dateTimePicker1.TabIndex = 6;
+            // 
             // previewControl1
             // 
             this.previewControl1.BackColor = System.Drawing.SystemColors.AppWorkspace;
@@ -109,59 +161,53 @@
             this.previewControl1.Size = new System.Drawing.Size(966, 610);
             this.previewControl1.TabIndex = 2;
             // 
-            // tableLayoutPanel1
+            // tabPage2
             // 
-            this.tableLayoutPanel1.BackColor = System.Drawing.Color.LightGreen;
-            this.tableLayoutPanel1.ColumnCount = 7;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 246F));
-            this.tableLayoutPanel1.Controls.Add(this.button1, 2, 1);
-            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.dateTimePicker1, 1, 1);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 4;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(966, 103);
-            this.tableLayoutPanel1.TabIndex = 2;
+            this.tabPage2.Location = new System.Drawing.Point(4, 26);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(976, 731);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // dateTimePicker1
+            // label1
             // 
-            this.dateTimePicker1.CustomFormat = "yyyy/MM";
-            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker1.Location = new System.Drawing.Point(123, 43);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(114, 27);
-            this.dateTimePicker1.TabIndex = 6;
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "年月起迄";
             // 
-            // button1
+            // dateTimePicker2
             // 
-            this.button1.Location = new System.Drawing.Point(243, 43);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(114, 34);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "查詢";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.dateTimePicker2.CustomFormat = "yyyy/MM";
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker2.Location = new System.Drawing.Point(123, 43);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(114, 27);
+            this.dateTimePicker2.TabIndex = 8;
             // 
-            // label3
+            // dateTimePicker3
             // 
-            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(3, 52);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(114, 16);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "年月";
+            this.dateTimePicker3.CustomFormat = "yyyy/MM";
+            this.dateTimePicker3.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker3.Location = new System.Drawing.Point(243, 43);
+            this.dateTimePicker3.Name = "dateTimePicker3";
+            this.dateTimePicker3.Size = new System.Drawing.Size(114, 27);
+            this.dateTimePicker3.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(363, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 34);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "查詢";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // frmREPORTENVTAXS
             // 
@@ -170,9 +216,10 @@
             this.ClientSize = new System.Drawing.Size(984, 761);
             this.Controls.Add(this.tabControl1);
             this.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmREPORTENVTAXS";
             this.Text = "frmREPORTENVTAXS";
+            this.Load += new System.EventHandler(this.frmREPORTENVTAXS_Load);
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.splitContainer1.Panel1.ResumeLayout(false);
@@ -196,5 +243,9 @@
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker3;
+        private System.Windows.Forms.Button button2;
     }
 }
--- TKPUR/frmREPORTENVTAXS.cs
+++ TKPUR/frmREPORTENVTAXS.cs
@@ -54,6 +54,28 @@ namespace TKPUR
         }
 
         #region FUNCTION
+       
+        private void frmREPORTENVTAXS_Load(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            int month = now.Month;
+            int year = now.Year;
+
+            // 计算当前月份属于哪个双月区间
+            int startMonth = ((month - 1) / 2) * 2 + 1; // 1, 3, 5, 7, 9, 11
+            int endMonth = startMonth + 1;              // 2, 4, 6, 8, 10, 12
+
+            DateTime lastMonth = new DateTime(year, startMonth, 1);
+            DateTime nowMonth = new DateTime(year, endMonth, 1);
+
+            dateTimePicker2.Value = lastMonth;
+            dateTimePicker3.Value = nowMonth;
+
+        }
+        public void SETDATE()
+        {
+
+        }
         public void SETFASTREPORT(string YYYYMM)
         {
             string YY = YYYYMM.Substring(0,4);
@@ -178,6 +200,151 @@ namespace TKPUR
                                 ORDER BY SUBSTRING(TG003,1,4),SUBSTRING(TG003,5,2),TG004,MA002,MA010,TH004
 
                                     ", YY, MM, STRQUERY.ToString());
+
+            return FASTSQL.ToString();
+        }
+
+        public void SETFASTREPORT2(string STARTYYYYMM,string ENDYYYYMM)
+        {
+            string STARTYY = STARTYYYYMM.Substring(0, 4);
+            string STARTMM = STARTYYYYMM.Substring(4, 2);
+            string ENDYY = ENDYYYYMM.Substring(0, 4);
+            string ENDMM = ENDYYYYMM.Substring(4, 2);
+
+            string SQL;
+            string SQL1;
+            report1 = new Report();
+            report1.Load(@"REPORT\環保稅.frx");
+
+            //20210902密
+            Class1 TKID = new Class1();//用new 建立類別實體
+            SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString);
+
+            //資料庫使用者密碼解密
+            sqlsb.Password = TKID.Decryption(sqlsb.Password);
+            sqlsb.UserID = TKID.Decryption(sqlsb.UserID);
+
+            String connectionString;
+            sqlConn = new SqlConnection(sqlsb.ConnectionString);
+
+            report1.Dictionary.Connections[0].ConnectionString = sqlsb.ConnectionString;
+
+
+            //report1.Dictionary.Connections[0].ConnectionString = "server=192.168.1.105;database=TKPUR;uid=sa;pwd=dsc";
+
+            TableDataSource Table = report1.GetDataSource("Table") as TableDataSource;
+            SQL = SETFASETSQL2A(STARTYY, STARTMM, ENDYY, ENDMM);
+            Table.SelectCommand = SQL;
+            TableDataSource Table1 = report1.GetDataSource("Table1") as TableDataSource;
+            SQL1 = SETFASETSQL2B(STARTYY, STARTMM, ENDYY, ENDMM);
+            Table1.SelectCommand = SQL1;
+            report1.Preview = previewControl1;
+            report1.Show();
+
+        }
+        public string SETFASETSQL2A(string STARTYY, string STARTMM, string ENDYY, string ENDMM)
+        {
+            StringBuilder FASTSQL = new StringBuilder();
+            StringBuilder STRQUERY = new StringBuilder();
+
+            DataTable DT = FIND_TKCOPMATAXSMB001PUR();
+            if (DT != null && DT.Rows.Count >= 1)
+            {
+                STRQUERY.AppendFormat(@" (");
+                int rowCount = DT.Rows.Count;
+
+                for (int i = 0; i < rowCount; i++)
+                {
+                    STRQUERY.AppendFormat(@" TH004 LIKE '{0}%'", DT.Rows[i]["MB001"].ToString());
+
+                    // 在最後一個元素之後不添加 "OR"
+                    if (i < rowCount - 1)
+                    {
+                        STRQUERY.AppendFormat(@" OR");
+                    }
+                }
+
+                STRQUERY.AppendFormat(@" )");
+            }
+
+
+            FASTSQL.AppendFormat(@"  
+                                
+                                SELECT 年, '{0}月' 月,廠商代,廠商,統編,品號,品名,SUM(進貨驗收數量) 進貨驗收數量,單位
+                                FROM
+                                (
+                                SELECT SUBSTRING(TG003,1,4) AS '年',SUBSTRING(TG003,5,2)  AS '月',TG005 AS '廠商代',MA002 AS '廠商',MA005 AS '統編',TH004 AS '品號',MB002 AS '品名',SUM(TH015)  AS '進貨驗收數量',TH008 AS '單位'
+                                FROM [TK].dbo.PURTG,[TK].dbo.PURTH, [TK].dbo.PURMA, [TK].dbo.INVMB 
+                                WHERE TG001=TH001 AND TG002=TH002
+                                AND MA001=TG005
+                                AND MB001=TH004
+                                AND TG013='Y'
+                                AND   ( TH004 LIKE '205%' OR TH004 LIKE '214%' OR TH004 LIKE '41004070020001%' OR TH004 LIKE '503001002001%' OR TH004 LIKE '503001002002%' OR TH004 LIKE '503001002003%' OR TH004 LIKE '503001002004%' ) 
+                                AND SUBSTRING(TG003,1,4)='{1}'
+                                AND SUBSTRING(TG003,5,2)>='{2}'
+                                AND SUBSTRING(TG003,5,2)<='{3}'
+                                AND  {4} 
+                                GROUP BY  SUBSTRING(TG003,1,4),SUBSTRING(TG003,5,2),TG005,MA002,MA005,TH004,MB002,MB003,TH008
+                                ) AS TEMP
+                                GROUP BY 年, 廠商代,廠商,統編,品號,品名,單位
+                                ORDER BY 年, 廠商代,廠商,統編,品號,品名,單位
+     
+                                    ", STARTMM+"-"+ ENDMM, STARTYY, STARTMM, ENDMM, STRQUERY.ToString());
+
+            return FASTSQL.ToString();
+        }
+        public string SETFASETSQL2B(string STARTYY, string STARTMM, string ENDYY, string ENDMM)
+        {
+            StringBuilder FASTSQL = new StringBuilder();
+            StringBuilder STRQUERY = new StringBuilder();
+
+            DataTable DT = FIND_TKCOPMATAXSMB001COP();
+            if (DT != null && DT.Rows.Count >= 1)
+            {
+                STRQUERY.AppendFormat(@" (");
+                int rowCount = DT.Rows.Count;
+
+                for (int i = 0; i < rowCount; i++)
+                {
+                    STRQUERY.AppendFormat(@" MD003 LIKE '{0}%'", DT.Rows[i]["MB001"].ToString());
+
+                    // 在最後一個元素之後不添加 "OR"
+                    if (i < rowCount - 1)
+                    {
+                        STRQUERY.AppendFormat(@" OR");
+                    }
+                }
+
+                STRQUERY.AppendFormat(@" )");
+            }
+
+            FASTSQL.AppendFormat(@"                                
+                              
+                                    SELECT 年, '{0}月' 月,客戶代,客戶,統編,品號,品名,SUM(數量) 數量,單位
+                                    FROM
+                                    (
+                                    SELECT  SUBSTRING(TG003,1,4) AS '年',SUBSTRING(TG003,5,2)  AS '月',TG004 AS '客戶代',MA002 AS '客戶',MA010 AS '統編',TH004 ,MB1.MB002 ,SUM(LA011) LA011,MB1.MB004,MC004,MD006,MD007,MD003 AS '品號',MB2.MB002 AS '品名',SUM(CONVERT(DECIMAL(16,0),(LA011/MD006*MD007*MC004)))  AS '數量',MB2.MB004 AS '單位'
+                                    FROM [TK].dbo.COPTG,[TK].dbo.COPTH,[TK].dbo.INVLA,[TK].dbo.INVMB MB1,[TK].dbo.COPMA,[TK].dbo.BOMMC,[TK].dbo.BOMMD,[TK].dbo.INVMB MB2
+                                    WHERE TG001=TH001 AND TG002=TH002
+                                    AND LA006=TH001 AND LA007=TH002 AND LA008=TH003
+                                    AND TH004=MB1.MB001
+                                    AND TG004=MA001
+                                    AND MC001=TH004
+                                    AND MC001=MD001
+                                    AND MD003=MB2.MB001
+                                    AND {4}
+                                    AND MD035 NOT LIKE '%蓋%'
+                                    AND (TG004 LIKE '2%' OR TG004 LIKE 'A%')
+                                    AND TG004 IN (SELECT  [MA001] FROM [TKPUR].[dbo].[TKCOPMATAXS])
+                                    AND SUBSTRING(TG003,1,4)='{1}' 
+                                    AND SUBSTRING(TG003,5,2)>='{2}'
+                                    AND SUBSTRING(TG003,5,2)>='{3}'
+                                    GROUP BY SUBSTRING(TG003,1,4),SUBSTRING(TG003,5,2),TG004,MA002,MA010,TH004,MB1.MB002,MB1.MB004,MC004,MD006,MD007,MD003,MB2.MB002,MB2.MB004
+                                    ) AS TEMP
+                                    GROUP BY 年, 客戶代,客戶,統編,品號,品名,單位
+                                    ORDER BY 年, 客戶代,客戶,統編,品號,品名,單位
+
+                                    ", STARTMM + "-" + ENDMM, STARTYY, STARTMM, ENDMM, STRQUERY.ToString());
 
             return FASTSQL.ToString();
         }
@@ -314,7 +481,13 @@ namespace TKPUR
         {
             SETFASTREPORT(dateTimePicker1.Value.ToString("yyyyMM"));
         }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SETFASTREPORT2(dateTimePicker2.Value.ToString("yyyyMM"), dateTimePicker3.Value.ToString("yyyyMM"));
+        }
 
         #endregion
+
+        
     }
 }
