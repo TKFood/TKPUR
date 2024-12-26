5720ad65175816b7fd8025a3131b5fd83507f776
at3158 <tk290@tkfood.com.tw>
2024-12-26 09:54:40 +0800

    Search

---
+++ TKPUR/FrmPURTGTPURTHCHECK.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+using NPOI.SS.UserModel;
+using System.Configuration;
+using NPOI.XSSF.UserModel;
+using NPOI.SS.Util;
+using System.Reflection;
+using System.Threading;
+using FastReport;
+using FastReport.Data;
+using TKITDLL;
+using FastReport.Export.Pdf;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Diagnostics;
+
+namespace TKPUR
+{
+    public partial class FrmPURTGTPURTHCHECK : Form
+    {
+        SqlConnection sqlConn = new SqlConnection();
+        SqlCommand sqlComm = new SqlCommand();
+        string connectionString;
+        StringBuilder sbSql = new StringBuilder();
+        StringBuilder sbSqlQuery = new StringBuilder();
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder = new SqlCommandBuilder();
+        SqlDataAdapter adapter2 = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder2 = new SqlCommandBuilder();
+        SqlDataAdapter adapter4 = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder4 = new SqlCommandBuilder();
+        SqlTransaction tran;
+        SqlCommand cmd = new SqlCommand();
+        DataSet ds = new DataSet();
+        DataSet ds2 = new DataSet();
+        DataSet ds4 = new DataSet();
+
+       
+        DataTable dt = new DataTable();
+        string tablename = null;
+        string EDITID;
+        int result;
+        Thread TD;
+
+        Report report1 = new Report();
+
+
+        public FrmPURTGTPURTHCHECK()
+        {
+            InitializeComponent();
+        }
+
+        #region FUNCTION
+        public void Search(string SDAY, string EDAY)
+        {
+            DataSet ds = new DataSet();
+
+            try
+            {
+                //20210902密
+                Class1 TKID = new Class1();//用new 建立類別實體
+                SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString);
+
+                //資料庫使用者密碼解密
+                sqlsb.Password = TKID.Decryption(sqlsb.Password);
+                sqlsb.UserID = TKID.Decryption(sqlsb.UserID);
+
+                String connectionString;
+                sqlConn = new SqlConnection(sqlsb.ConnectionString);
+
+
+
+                sbSql.Clear();
+                sbSqlQuery.Clear();
+
+
+                sbSql.AppendFormat(@"                                    
+                                    SELECT 
+                                    TG001 AS '單別'
+                                    ,TG002 AS '單號'
+                                    ,TG003 AS '進貨日期'
+                                    ,TG005 AS '供應廠商'
+                                    ,TG021 AS '廠商全名'
+                                    ,TG011 AS '發票號碼'
+                                    ,TG027 AS '發票日期'
+                                    ,TG022 AS '統一編號'
+                                    ,(CASE WHEN TG010=1 THEN '應稅內含' 
+                                    WHEN TG010=2 THEN '應稅外加' 
+                                    WHEN TG010=3 THEN '零稅率' 
+                                    WHEN TG010=4 THEN '免稅' 
+                                    WHEN TG010=9 THEN '不計稅' 
+                                    END) AS '課稅別'
+                                    ,TG031 AS '本幣貨款金額'
+                                    ,TG032 AS '本幣稅額'
+                                    FROM [TK].dbo.PURTG
+                                    WHERE TG003>='{0}' AND TG003<='{1}'
+
+
+                                    ", SDAY, EDAY);
+
+                adapter = new SqlDataAdapter(@"" + sbSql, sqlConn);
+
+                sqlCmdBuilder = new SqlCommandBuilder(adapter);
+                sqlConn.Open();
+                ds.Clear();
+                adapter.Fill(ds, "TEMPds1");
+                sqlConn.Close();
+
+
+                if (ds.Tables["TEMPds1"].Rows.Count == 0)
+                {
+                    dataGridView1.DataSource = null;
+                }
+                else
+                {
+                    if (ds.Tables["TEMPds1"].Rows.Count >= 1)
+                    {
+                        dataGridView1.DataSource = ds.Tables["TEMPds1"];
+                        dataGridView1.AutoResizeColumns();
+
+
+                    }
+
+                }
+
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+        }
+        #endregion
+
+        #region BUTTON
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Search(dateTimePicker1.Value.ToString("yyyyMMdd"), dateTimePicker2.Value.ToString("yyyyMMdd"));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+
+
+    }
+}
