49f1e5b6696e408554cd365f91b8625cf5cfcc97
at3158 <tk290@tkfood.com.tw>
2025-03-10 14:03:56 +0800

    SETSQL

--- TKPUR/FrmREPURPRICE.cs
+++ TKPUR/FrmREPURPRICE.cs
@@ -82,38 +82,37 @@ namespace TKPUR
         public StringBuilder SETSQL()
         {
             StringBuilder SB = new StringBuilder();
-
-            SB.AppendFormat(" SELECT TH004,TH005,TH018,TH008");
-            SB.AppendFormat(" ,(SELECT SUM(TH007) FROM [TK].dbo.PURTG TG ,[TK].dbo.PURTH TH WHERE TG.TG001=TH.TH001 AND TG.TG002=TH.TH002 AND  TG.TG003>='{0}' AND TG.TG003<='{1}' AND TH.TH004=TEMP.TH004 AND TH.TH008=TEMP.TH008 ) AS 'NUM'", dateTimePicker1.Value.ToString("yyyyMMdd"), dateTimePicker2.Value.ToString("yyyyMMdd"));
-            SB.AppendFormat(" ,(SELECT TOP 1 SUBSTRING(TG003,1,6) FROM [TK].dbo.PURTG TG ,[TK].dbo.PURTH TH WHERE TG.TG001=TH.TH001 AND TG.TG002=TH.TH002 AND  TG003>='{0}' AND TG003<='{1}' AND TH.TH004=TEMP.TH004 AND TH.TH018=TEMP.TH018) AS 'YM'",dateTimePicker1.Value.ToString("yyyyMMdd"), dateTimePicker2.Value.ToString("yyyyMMdd"));
-            SB.AppendFormat(" FROM (");
-            SB.AppendFormat(" SELECT  TH004,TH005,TH018,TH008");
-            SB.AppendFormat(" FROM [TK].dbo.PURTG,[TK].dbo.PURTH");
-            SB.AppendFormat(" WHERE TG001=TH001 AND TG002=TH002");
-            SB.AppendFormat(" AND ( TH004 LIKE '1%' OR  TH004 LIKE '2%')");
-            SB.AppendFormat(" AND TH004 NOT  LIKE '199%'");
-            SB.AppendFormat(" AND TH004 NOT  LIKE '299%'");
-            SB.AppendFormat(" AND  TG003>='{0}' AND TG003<='{1}'", dateTimePicker1.Value.ToString("yyyyMMdd"), dateTimePicker2.Value.ToString("yyyyMMdd"));
-            SB.AppendFormat(" GROUP BY  TH004,TH005,TH018,TH008");
-            SB.AppendFormat(" ) AS TEMP");
-            SB.AppendFormat(" WHERE TH004 IN (");
-            SB.AppendFormat(" SELECT  TH004");
-            SB.AppendFormat(" FROM (");
-            SB.AppendFormat(" SELECT  TH004,TH005,TH018,TH008");
-            SB.AppendFormat(" FROM [TK].dbo.PURTG,[TK].dbo.PURTH");
-            SB.AppendFormat(" WHERE TG001=TH001 AND TG002=TH002");
-            SB.AppendFormat(" AND ( TH004 LIKE '1%' OR  TH004 LIKE '2%')");
-            SB.AppendFormat(" AND TH004 NOT  LIKE '199%'");
-            SB.AppendFormat(" AND TH004 NOT  LIKE '299%'");
-            SB.AppendFormat(" AND  TG003>='{0}' AND TG003<='{1}'", dateTimePicker1.Value.ToString("yyyyMMdd"), dateTimePicker2.Value.ToString("yyyyMMdd"));
-            SB.AppendFormat(" GROUP BY  TH004,TH005,TH018,TH008");
-            SB.AppendFormat(" ) AS TEMP");
-            SB.AppendFormat(" GROUP BY TH004,TH005,TH008");
-            SB.AppendFormat(" HAVING COUNT(TH004)>=2");
-            SB.AppendFormat(" )");
-            SB.AppendFormat(" ORDER BY TH004,(SELECT TOP 1 SUBSTRING(TG003,1,6) FROM [TK].dbo.PURTG TG ,[TK].dbo.PURTH TH WHERE TG.TG001=TH.TH001 AND TG.TG002=TH.TH002 AND  TG003>='{0}' AND TG003<='{1}' AND TH.TH004=TEMP.TH004 AND TH.TH018=TEMP.TH018)", dateTimePicker1.Value.ToString("yyyyMMdd"), dateTimePicker2.Value.ToString("yyyyMMdd"));
-            SB.AppendFormat("  ");
-            SB.AppendFormat(" ");
+            SB.AppendFormat(@" 
+                            SELECT 
+                            TH004,TH005,TH018,TH008
+                            ,(SELECT SUM(TH007) FROM [TK].dbo.PURTG TG ,[TK].dbo.PURTH TH WHERE TG.TG001=TH.TH001 AND TG.TG002=TH.TH002 AND  TG.TG003>='{0}' AND TG.TG003<='{1}' AND TH.TH004=TEMP.TH004 AND TH.TH008=TEMP.TH008 ) AS 'NUM'
+                            ,(SELECT TOP 1 SUBSTRING(TG003, 1, 6) FROM[TK].dbo.PURTG TG ,[TK].dbo.PURTH TH WHERE TG.TG001=TH.TH001 AND TG.TG002= TH.TH002 AND TG003>='{0}' AND TG003<='{1}' AND TH.TH004= TEMP.TH004 AND TH.TH018= TEMP.TH018) AS 'YM'
+                            FROM(
+                            SELECT TH004, TH005, TH018, TH008
+                            FROM [TK].dbo.PURTG, [TK].dbo.PURTH
+                            WHERE TG001= TH001 AND TG002 = TH002
+                            AND ( TH004 LIKE '1%' OR TH004 LIKE '2%')
+                            AND TH004 NOT LIKE '199%'
+                            AND TH004 NOT LIKE '299%'
+                            AND TG003>='{0}' AND TG003<='{1}'
+                            GROUP BY  TH004,TH005,TH018,TH008
+                            ) AS TEMP
+                            WHERE TH004 IN(
+                            SELECT TH004
+                            FROM (
+                            SELECT TH004, TH005, TH018, TH008
+                            FROM [TK].dbo.PURTG, [TK].dbo.PURTH
+                            WHERE TG001= TH001 AND TG002 = TH002
+                            AND ( TH004 LIKE '1%' OR TH004 LIKE '2%')
+                            AND TH004 NOT LIKE '199%'
+                            AND TH004 NOT LIKE '299%'
+                            AND TG003>='{0}' AND TG003<='{1}'
+                            GROUP BY  TH004,TH005,TH018,TH008
+                            ) AS TEMP
+                            GROUP BY TH004,TH005,TH008
+                            HAVING COUNT(TH004)>=2
+                            )ORDER BY TH004,(SELECT TOP 1 SUBSTRING(TG003,1,6) FROM[TK].dbo.PURTG TG,[TK].dbo.PURTH TH WHERE TG.TG001=TH.TH001 AND TG.TG002= TH.TH002 AND TG003>='{0}' AND TG003<='{1}' AND TH.TH004= TEMP.TH004 AND TH.TH018= TEMP.TH018)
+                            ", dateTimePicker1.Value.ToString("yyyyMMdd"), dateTimePicker2.Value.ToString("yyyyMMdd"));
 
             return SB;
 
