27c723f017323e28d8fb4fcec9bc16367ed7857a
at3158 <tk290@tkfood.com.tw>
2025-03-10 14:33:53 +0800

    SETSQL

--- TKPUR/FrmREPURPRICE.Designer.cs
+++ TKPUR/FrmREPURPRICE.Designer.cs
@@ -30,7 +30,6 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
@@ -39,6 +38,9 @@
             this.label7 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.previewControl1 = new FastReport.Preview.PreviewControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
@@ -54,7 +56,7 @@
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
-            this.tabControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(1139, 737);
@@ -64,24 +66,13 @@
             // 
             this.tabPage1.Controls.Add(this.splitContainer1);
             this.tabPage1.Location = new System.Drawing.Point(4, 26);
-            this.tabPage1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4);
             this.tabPage1.Size = new System.Drawing.Size(1131, 707);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.tabPage2.Size = new System.Drawing.Size(1468, 957);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // splitContainer1
             // 
@@ -99,7 +90,7 @@
             // 
             this.splitContainer1.Panel2.Controls.Add(this.previewControl1);
             this.splitContainer1.Size = new System.Drawing.Size(1123, 699);
-            this.splitContainer1.SplitterDistance = 173;
+            this.splitContainer1.SplitterDistance = 151;
             this.splitContainer1.TabIndex = 0;
             // 
             // tableLayoutPanel4
@@ -108,9 +99,9 @@
             this.tableLayoutPanel4.ColumnCount = 12;
             this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
             this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
-            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
-            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
-            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 59F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 121F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 270F));
             this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
             this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 174F));
             this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 180F));
@@ -118,11 +109,13 @@
             this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 102F));
             this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
             this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
-            this.tableLayoutPanel4.Controls.Add(this.dateTimePicker1, 1, 1);
             this.tableLayoutPanel4.Controls.Add(this.button1, 4, 1);
-            this.tableLayoutPanel4.Controls.Add(this.dateTimePicker2, 3, 1);
-            this.tableLayoutPanel4.Controls.Add(this.label7, 0, 1);
-            this.tableLayoutPanel4.Controls.Add(this.label1, 2, 1);
+            this.tableLayoutPanel4.Controls.Add(this.label7, 0, 0);
+            this.tableLayoutPanel4.Controls.Add(this.dateTimePicker1, 1, 0);
+            this.tableLayoutPanel4.Controls.Add(this.dateTimePicker2, 3, 0);
+            this.tableLayoutPanel4.Controls.Add(this.label1, 2, 0);
+            this.tableLayoutPanel4.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel4.Controls.Add(this.textBox1, 1, 1);
             this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel4.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel4.Margin = new System.Windows.Forms.Padding(4);
@@ -132,7 +125,7 @@
             this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
             this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
             this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
-            this.tableLayoutPanel4.Size = new System.Drawing.Size(1119, 169);
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(1119, 147);
             this.tableLayoutPanel4.TabIndex = 16;
             // 
             // dateTimePicker1
@@ -140,7 +133,7 @@
             this.dateTimePicker1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.dateTimePicker1.CustomFormat = "yyyy/MM/dd";
             this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker1.Location = new System.Drawing.Point(154, 46);
+            this.dateTimePicker1.Location = new System.Drawing.Point(154, 6);
             this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(142, 27);
@@ -149,10 +142,10 @@
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(604, 44);
+            this.button1.Location = new System.Drawing.Point(484, 44);
             this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(142, 32);
+            this.button1.Size = new System.Drawing.Size(262, 32);
             this.button1.TabIndex = 16;
             this.button1.Text = "查詢";
             this.button1.UseVisualStyleBackColor = true;
@@ -163,17 +156,17 @@
             this.dateTimePicker2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.dateTimePicker2.CustomFormat = "yyyy/MM/dd";
             this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker2.Location = new System.Drawing.Point(454, 46);
+            this.dateTimePicker2.Location = new System.Drawing.Point(363, 6);
             this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(4);
             this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(142, 27);
+            this.dateTimePicker2.Size = new System.Drawing.Size(113, 27);
             this.dateTimePicker2.TabIndex = 25;
             // 
             // label7
             // 
             this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(4, 52);
+            this.label7.Location = new System.Drawing.Point(4, 12);
             this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(142, 16);
@@ -185,10 +178,10 @@
             // 
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(304, 52);
+            this.label1.Location = new System.Drawing.Point(304, 12);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(142, 16);
+            this.label1.Size = new System.Drawing.Size(51, 16);
             this.label1.TabIndex = 26;
             this.label1.Text = "~";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -203,8 +196,39 @@
             this.previewControl1.Name = "previewControl1";
             this.previewControl1.PageOffset = new System.Drawing.Point(10, 10);
             this.previewControl1.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.previewControl1.Size = new System.Drawing.Size(1119, 518);
+            this.previewControl1.Size = new System.Drawing.Size(1119, 540);
             this.previewControl1.TabIndex = 9;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 26);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(4);
+            this.tabPage2.Size = new System.Drawing.Size(1131, 707);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 52);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 16);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "查詢品號/品名";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(153, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(144, 27);
+            this.textBox1.TabIndex = 28;
             // 
             // FrmREPURPRICE
             // 
@@ -213,7 +237,7 @@
             this.ClientSize = new System.Drawing.Size(1139, 737);
             this.Controls.Add(this.tabControl1);
             this.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FrmREPURPRICE";
             this.Text = "FrmREPURPRICE";
             this.tabControl1.ResumeLayout(false);
@@ -241,5 +265,7 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label1;
         private FastReport.Preview.PreviewControl previewControl1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- TKPUR/FrmREPURPRICE.cs
+++ TKPUR/FrmREPURPRICE.cs
@@ -48,13 +48,13 @@ namespace TKPUR
         }
 
         #region FUNCTION
-        public void SETFASTREPORT()
+        public void SETFASTREPORT(string SDATES,string EDATES,string TH004)
         {
             StringBuilder SQL1 = new StringBuilder();
 
-            SQL1 = SETSQL();
+            SQL1 = SETSQL(SDATES, EDATES, TH004);
             Report report1 = new Report();
-            report1.Load(@"REPORT\原物料張跌表.frx");
+            report1.Load(@"REPORT\原物料漲跌表V2.frx");
 
             //20210902密
             Class1 TKID = new Class1();//用new 建立類別實體
@@ -79,40 +79,47 @@ namespace TKPUR
             report1.Show();
         }
 
-        public StringBuilder SETSQL()
+        public StringBuilder SETSQL(string SDATES, string EDATES, string TH004)
         {
             StringBuilder SB = new StringBuilder();
-            SB.AppendFormat(@" 
-                            SELECT 
-                            TH004,TH005,TH018,TH008
-                            ,(SELECT SUM(TH007) FROM [TK].dbo.PURTG TG ,[TK].dbo.PURTH TH WHERE TG.TG001=TH.TH001 AND TG.TG002=TH.TH002 AND  TG.TG003>='{0}' AND TG.TG003<='{1}' AND TH.TH004=TEMP.TH004 AND TH.TH008=TEMP.TH008 ) AS 'NUM'
-                            ,(SELECT TOP 1 SUBSTRING(TG003, 1, 6) FROM[TK].dbo.PURTG TG ,[TK].dbo.PURTH TH WHERE TG.TG001=TH.TH001 AND TG.TG002= TH.TH002 AND TG003>='{0}' AND TG003<='{1}' AND TH.TH004= TEMP.TH004 AND TH.TH018= TEMP.TH018) AS 'YM'
-                            FROM(
-                            SELECT TH004, TH005, TH018, TH008
-                            FROM [TK].dbo.PURTG, [TK].dbo.PURTH
-                            WHERE TG001= TH001 AND TG002 = TH002
-                            AND ( TH004 LIKE '1%' OR TH004 LIKE '2%')
-                            AND TH004 NOT LIKE '199%'
-                            AND TH004 NOT LIKE '299%'
-                            AND TG003>='{0}' AND TG003<='{1}'
-                            GROUP BY  TH004,TH005,TH018,TH008
-                            ) AS TEMP
-                            WHERE TH004 IN(
-                            SELECT TH004
-                            FROM (
-                            SELECT TH004, TH005, TH018, TH008
-                            FROM [TK].dbo.PURTG, [TK].dbo.PURTH
-                            WHERE TG001= TH001 AND TG002 = TH002
-                            AND ( TH004 LIKE '1%' OR TH004 LIKE '2%')
-                            AND TH004 NOT LIKE '199%'
-                            AND TH004 NOT LIKE '299%'
-                            AND TG003>='{0}' AND TG003<='{1}'
-                            GROUP BY  TH004,TH005,TH018,TH008
-                            ) AS TEMP
-                            GROUP BY TH004,TH005,TH008
-                            HAVING COUNT(TH004)>=2
-                            )ORDER BY TH004,(SELECT TOP 1 SUBSTRING(TG003,1,6) FROM[TK].dbo.PURTG TG,[TK].dbo.PURTH TH WHERE TG.TG001=TH.TH001 AND TG.TG002= TH.TH002 AND TG003>='{0}' AND TG003<='{1}' AND TH.TH004= TEMP.TH004 AND TH.TH018= TEMP.TH018)
-                            ", dateTimePicker1.Value.ToString("yyyyMMdd"), dateTimePicker2.Value.ToString("yyyyMMdd"));
+            StringBuilder SBQUERY1 = new StringBuilder();
+
+            if(!string.IsNullOrEmpty(TH004))
+            {
+                SBQUERY1.AppendFormat(@"
+                                        AND (TH004 LIKE '%{0}%' OR TH005 LIKE '%{0}%')
+                                        ", TH004);
+            }
+            else
+            {
+                SBQUERY1.AppendFormat(@"
+                                       
+                                        ");
+            }
+            
+            SB.AppendFormat(@"                            
+                                SELECT 
+                                SUBSTRING(TG003,1,6) AS 'YM'
+                                ,TH004 AS '品號'
+                                ,TH005 AS '品名'
+                                ,TH008 AS '單位'
+                                ,SUM(TH007) AS '進貨數量'
+                                ,SUM(TH016) AS '計價數量'
+                                ,SUM(TH047+TH048) AS '本幣金額'
+                                ,(CASE WHEN SUM(TH047+TH048)>0 AND SUM(TH016)>0 THEN SUM(TH047+TH048)/SUM(TH016) ELSE 0 END )  AS '進貨單價'
+                                ,(SELECT SUM(TH007) FROM [TK].dbo.PURTG TG ,[TK].dbo.PURTH TH WHERE TG.TG001=TH.TH001 AND TG.TG002=TH.TH002 AND  TG.TG003>='20240101' AND TG.TG003<='20250228' AND TH.TH004=PURTH.TH004 AND TH.TH008=PURTH.TH008 ) AS '進貨總數量'
+
+                                FROM [TK].dbo.PURTG,[TK].dbo.PURTH
+                                WHERE TG001=TH001
+                                AND TG002=TH002
+                                AND TG013='Y'
+                                AND TH004 NOT LIKE '199%'
+                                AND TH004 NOT LIKE '299%'
+                                AND TG003>='{0}' AND TG003<='{1}'
+                                {2}
+
+                                GROUP BY SUBSTRING(TG003,1,6) ,TH004,TH005,TH008
+                            ", dateTimePicker1.Value.ToString("yyyyMMdd"), dateTimePicker2.Value.ToString("yyyyMMdd"), SBQUERY1.ToString());
 
             return SB;
 
@@ -124,7 +131,7 @@ namespace TKPUR
 
         private void button1_Click(object sender, EventArgs e)
         {
-            SETFASTREPORT();
+            SETFASTREPORT(dateTimePicker1.Value.ToString("yyyyMMdd"), dateTimePicker2.Value.ToString("yyyyMMdd"),textBox1.Text.Trim());
         }
         #endregion
     }
